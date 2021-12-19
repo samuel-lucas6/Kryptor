585cb8bb8e467fcd94503d6ac1688f7883cea759
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2021-12-19 14:47:31 +0000

    src: Please fix the CodeQL error.
    I forgot to copy the Properties folder.

--- src/KryptorCLI/Properties/Resources.Designer.cs
+++ src/KryptorCLI/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace KryptorCLI.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,26 @@ namespace KryptorCLI.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] vcruntime140x64 {
+            get {
+                object obj = ResourceManager.GetObject("vcruntime140x64", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] vcruntime140x86 {
+            get {
+                object obj = ResourceManager.GetObject("vcruntime140x86", resourceCulture);
+                return ((byte[])(obj));
             }
         }
         
