0d17ece640f997470fff21a578425c003512add6
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2022-04-24 12:57:50 +0100

    src: Please fix the hotfix build issue.

--- src/KryptorCLI/Properties/Resources.Designer.cs
+++ src/KryptorCLI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Kryptor.Properties {
+namespace KryptorCLI.Properties {
     using System;
     
     
