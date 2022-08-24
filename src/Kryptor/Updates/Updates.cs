f446c2e658646052d2998dbb483d6ab66e25edbd
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2022-08-24 12:00:37 +0100

    Updates.cs: Support ARM64.

--- src/Kryptor/Updates/Updates.cs
+++ src/Kryptor/Updates/Updates.cs
@@ -17,38 +17,37 @@
 */
 
 using System;
-using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
 using System.Net;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
 namespace Kryptor;
 
 public static class Updates
 {
-    private static readonly string LocalApplicationDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-    private const string VersionFileName = "version.txt";
-    private const string VersionFileLink = "https://raw.githubusercontent.com/samuel-lucas6/Kryptor/master/version.txt";
-    private const string LatestReleaseFileName = "kryptor-latest.zip";
-    private const string WindowsDownloadFileName = "kryptor-windows.zip";
-    private const string LinuxDownloadFileName = "kryptor-linux.zip";
-    private const string MacOSDownloadFileName = "kryptor-macos.zip";
+    private static readonly string LocalAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
     private const string ExecutableFileName = "kryptor";
+    private const string WindowsExecutableFileName = $"{ExecutableFileName}.exe";
 
     public static bool CheckForUpdates(out string latestVersion)
     {
         string assemblyVersion = Program.GetVersion();
         latestVersion = GetLatestVersion();
-        return new Version(latestVersion).CompareTo(new Version(assemblyVersion)) == 1;
+        return new Version(latestVersion).CompareTo(new Version(assemblyVersion)) > 0;
     }
 
     private static string GetLatestVersion()
     {
-        string downloadFilePath = Path.Combine(LocalApplicationDataPath, VersionFileName);
+        const string versionFileLink = "https://raw.githubusercontent.com/samuel-lucas6/Kryptor/master/version.txt";
+        string downloadFilePath = Path.Combine(LocalAppDataPath, Path.GetFileName(versionFileLink));
         string signatureFilePath = downloadFilePath + Constants.SignatureExtension;
-        DownloadFile(VersionFileLink + Constants.SignatureExtension, signatureFilePath);
-        DownloadFile(VersionFileLink, downloadFilePath);
+        
+        DownloadFile(versionFileLink + Constants.SignatureExtension, signatureFilePath);
+        DownloadFile(versionFileLink, downloadFilePath);
+        
         string latestVersion = File.ReadAllText(downloadFilePath).Trim('\n').Trim();
         VerifyDownloadSignature(signatureFilePath, downloadFilePath, latestVersion);
         File.Delete(downloadFilePath);
@@ -63,8 +62,8 @@ public static class Updates
 
     public static void Update(string latestVersion)
     {
-        if (!Environment.Is64BitOperatingSystem || !OperatingSystem.IsWindows() & !OperatingSystem.IsLinux() & !OperatingSystem.IsMacOS()) {
-            throw new PlatformNotSupportedException("There are no official releases for your operating system.");
+        if (RuntimeInformation.OSArchitecture is not (Architecture.X64 or Architecture.Arm64) || !OperatingSystem.IsWindows() & !OperatingSystem.IsLinux() & !OperatingSystem.IsMacOS()) {
+            throw new PlatformNotSupportedException("There are no official releases for your operating system/architecture.");
         }
         Console.WriteLine("Downloading update...");
         byte[] downloadedExecutable = GetLatestExecutable(latestVersion);
@@ -74,19 +73,22 @@ public static class Updates
 
     private static byte[] GetLatestExecutable(string latestVersion)
     {
-        string downloadFilePath = Path.Combine(LocalApplicationDataPath, LatestReleaseFileName);
+        string downloadFilePath = Path.Combine(LocalAppDataPath, "kryptor-latest.zip");
         string downloadLink = GetReleaseDownloadLink(latestVersion);
         string signatureFilePath = downloadFilePath + Constants.SignatureExtension;
+        
         DownloadFile(downloadLink + Constants.SignatureExtension, signatureFilePath);
         DownloadFile(downloadLink, downloadFilePath);
         VerifyDownloadSignature(signatureFilePath, downloadFilePath, latestVersion);
-        string extractedDirectoryPath = Path.Combine(LocalApplicationDataPath, ExecutableFileName);
+        
+        string extractedDirectoryPath = Path.Combine(LocalAppDataPath, ExecutableFileName);
         if (!Directory.Exists(extractedDirectoryPath)) {
             Directory.CreateDirectory(extractedDirectoryPath);
         }
         ZipFile.ExtractToDirectory(downloadFilePath, extractedDirectoryPath, overwriteFiles: true);
         File.Delete(downloadFilePath);
-        string executableFilePath = Path.Combine(extractedDirectoryPath, OperatingSystem.IsWindows() ? $"{ExecutableFileName}.exe" : ExecutableFileName);
+        
+        string executableFilePath = Path.Combine(extractedDirectoryPath, OperatingSystem.IsWindows() ? WindowsExecutableFileName : ExecutableFileName);
         byte[] downloadedExecutable = File.ReadAllBytes(executableFilePath);
         Directory.Delete(extractedDirectoryPath, recursive: true);
         return downloadedExecutable;
@@ -95,9 +97,11 @@ public static class Updates
     private static string GetReleaseDownloadLink(string latestVersion)
     {
         string downloadLink = $"https://github.com/samuel-lucas6/Kryptor/releases/download/v{latestVersion}/";
-        if (OperatingSystem.IsWindows()) { return downloadLink + WindowsDownloadFileName; }
-        if (OperatingSystem.IsLinux()) { return downloadLink + LinuxDownloadFileName; }
-        if (OperatingSystem.IsMacOS()) { return downloadLink + MacOSDownloadFileName; }
+        if (OperatingSystem.IsWindows()) { return downloadLink + "kryptor-windows-amd64.zip"; }
+        if (OperatingSystem.IsLinux() && RuntimeInformation.OSArchitecture == Architecture.X64) { return downloadLink + "kryptor-linux-amd64.zip"; }
+        if (OperatingSystem.IsLinux() && RuntimeInformation.OSArchitecture == Architecture.Arm64) { return downloadLink + "kryptor-linux-arm64.zip"; }
+        if (OperatingSystem.IsMacOS() && RuntimeInformation.OSArchitecture == Architecture.X64) { return downloadLink + "kryptor-macos-amd64.zip"; }
+        if (OperatingSystem.IsMacOS() && RuntimeInformation.OSArchitecture == Architecture.Arm64) { return downloadLink + "kryptor-macos-arm64.zip"; }
         return null;
     }
 
@@ -106,7 +110,7 @@ public static class Updates
         Span<byte> publicKey = AsymmetricKeyValidation.SigningPublicKeyString("RWRudj7GpRdUxpojSmgHBOoNGUoD37H0WOUMAcT0yZcobg==");
         bool validSignature = DigitalSignatures.VerifySignature(signatureFilePath, downloadFilePath, publicKey, out string comment);
         File.Delete(signatureFilePath);
-        if (validSignature && string.Equals(comment, $"Kryptor v{latestVersion}")) {
+        if (validSignature && comment.Equals($"Kryptor v{latestVersion}")) {
             return;
         }
         File.Delete(downloadFilePath);
@@ -120,8 +124,8 @@ public static class Updates
             File.WriteAllBytes(executableFilePath, downloadedExecutable);
         }
         else {
-            string newExecutableFilePath = Path.Combine(LocalApplicationDataPath, $"{ExecutableFileName}.exe");
-            string batFilePath = Path.Combine(LocalApplicationDataPath, $"{ExecutableFileName}.bat");
+            string newExecutableFilePath = Path.Combine(LocalAppDataPath, WindowsExecutableFileName);
+            string batFilePath = Path.Combine(LocalAppDataPath, $"{ExecutableFileName}.bat");
             File.WriteAllBytes(newExecutableFilePath, downloadedExecutable);
             using (var batFile = new StreamWriter(File.Create(batFilePath)))
             {
@@ -139,6 +143,6 @@ public static class Updates
             Process.Start(startInfo);
         }
         DisplayMessage.WriteLine("Update complete. Please specify -a|--about to check the version.", ConsoleColor.Green);
-        Environment.Exit(0);
+        Environment.Exit(exitCode: 0);
     }
 }
